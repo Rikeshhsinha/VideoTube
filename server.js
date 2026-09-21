449076ea3305d85db4158e21056053678275e55b
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-22 02:43:11 +0530

    MongoDb connected Sucessfully

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+import dns from "dns";
+
+dns.setServers([
+    "8.8.8.8",
+    "1.1.1.1"
+]);
+
 import "dotenv/config.js";
 import app from "./src/app.js";
+import connectDB from "./src/db/db.js";
 
 const PORT = process.env.PORT || 4000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
