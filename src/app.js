a4aa154803d7f6bd03eab35a44c2f037152d9284
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-23 03:04:28 +0530

    Add utils codes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,25 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from cors;
+
+
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true
+}));
+
+app.use(express.json({
+    limit:"16kb"
+}));
+
+app.use(express.urlencoded({extended:true,
+    limit:"16kb"
+}));
+
+app.use(express.static("public"));
+app.use(cookieParser());
+
 
 const app = express();
 
