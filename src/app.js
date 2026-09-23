b0158d1e2a486e059dd0cb1c51e60bb306f259cb
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-23 15:37:07 +0530

    Add user Schema

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ app.use(express.urlencoded({extended:true,
 }));
 
 app.use(express.static("public"));
+
+
 app.use(cookieParser());
 
 
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,52 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudnary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-
+export const user = mongoose.model("User", userSchema);
