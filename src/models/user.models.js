b9c3ad97b61dfd35c5e3f3d5ba3fbf3a5816a84c
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-23 16:43:41 +0530

    Add user and video model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,8 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from bcrypt;
+import  JsonWebToken from "jsonwebtoken";
+import "dotenv/config.js";
+
 
 const userSchema = new Schema(
   {
@@ -48,5 +52,45 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next()
+
+        this.password = bcrypt.hash(this.password,10)
+        next()
+
+})
+
+userSchema.methods.isPasswordCorrect = async function 
+(password){
+     return await bcrypt.compare(password,this.password)
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+   return JsonWebToken.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SCERET,
+    {
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken = function () {
+     return JsonWebToken.sign({
+        _id:this._id,
+       
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const user = mongoose.model("User", userSchema);
